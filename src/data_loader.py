fe7c2ef85778345d0e5fb4fd852d3b563a1caf68
Goncalo Alcobia <goncaloalcobia@MacBook-Air-de-Goncalo.local>
2025-07-08 15:35:48 +0100

    First change in data_loader.py for version correction

--- src/data_loader.py
+++ src/data_loader.py
@@ -4,10 +4,10 @@ import yfinance as yf
 import pandas as pd
 
 def load_data(tickers, start, end):
-    data = yf.download(tickers, start=start, end=end)["Adj Close"]
+    raw_data = yf.download(tickers, start=start, end=end, group_by='ticker', auto_adjust=True)
+    data = pd.DataFrame({ticker: raw_data[ticker]["Close"] for ticker in tickers})
     data = data.dropna()
     return data
 
 def calculate_returns(price_df):
     return price_df.pct_change().dropna()
-
